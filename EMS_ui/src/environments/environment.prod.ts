faa0745f20b3c8e556b52d339ac43099f4482e05
pavan krishna <pavankrishnavedagiri@gmail.com>
2026-05-30 19:30:58 +0530

    Ui prod test

---
+++ EMS_ui/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: "https://estate-management-system-production.up.railway.app/estate-management-portal/api/v1",
+    authUrl: "https://estate-management-system-production.up.railway.app/estate-management-portal/api/v1/auth/login"
+}
